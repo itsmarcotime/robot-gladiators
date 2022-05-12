ce94dea738134417f5aba984d480a3ace791dec2
Marco Mata <patrickstar1281@yahoo.com>
2022-05-12 12:04:43 -0500

    expermenting with js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! ChichiScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("this logs a string, good for leaving a note");
+
+console.log(10 + 10);
+
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("the fight has begun!");
+}
+
+//fight();
+
