75d219aa0a2d6c3ea8204fb896a5e11b7c947cb0
Marco Mata <patrickstar1281@yahoo.com>
2022-05-14 01:30:28 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,11 +5,11 @@ var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "EVIL CHI";
+var enemyNames = ["EVIL CHI", "Chichibot", "Chirouble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
 
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
@@ -61,12 +61,14 @@ var fight = function() {
         } else {
             fight();
         }
-        
+
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
 
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
 
