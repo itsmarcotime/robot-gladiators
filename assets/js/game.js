cc988aeaef5a8fe3727b795950d82a0b94f47cda
Marco Mata <patrickstar1281@yahoo.com>
2022-05-15 06:35:47 -0500

    random health & attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,6 +7,13 @@ var enemyNames = ["EVIL CHI", "Chichibot", "Chirouble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+//function to generate random numeric value
+var randomNumber = function(min, max) {
+
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+};
 
 
 var fight = function (enemyName) {
@@ -27,7 +34,7 @@ var fight = function (enemyName) {
                 window.alert(playerName + " has decided to quit this fight. Goodbye!");
 
                 //subtract playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
 
                 console.log("playerMoney", playerMoney)
 
@@ -37,7 +44,10 @@ var fight = function (enemyName) {
         }
 
         // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        //generate random damage value based on players attack
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
 
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
@@ -57,7 +67,9 @@ var fight = function (enemyName) {
         }
 
         // remove player's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
 
         console.log(
             enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + "health remaining."
@@ -94,7 +106,7 @@ var startGame = function () {
             var pickedEnemyName = enemyNames[i];
 
             // reset enemyHealth before starting new round
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40,60);
 
             // pass the pickedEnemyName variables value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
